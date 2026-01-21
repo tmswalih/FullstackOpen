4395f318697a5f1272a48ea1016aba0e08bf9bda
*tm <tmswalih953@gmail.com>
2026-01-21 22:58:08 +0530

    Course information step 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,8 +1,8 @@
-const Header = () => {
-  const course = 'Half Stack application development'
+const Header = (props) => {
+  
   return(
     <div>
-      <h1>{course}</h1>
+      <h1>{props.course}</h1>
     </div>
   )
 }
@@ -43,16 +43,24 @@ const Total = () => {
 
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  
   
 
   return (
     
     <div>
 
-      <Header />
+      <Header course = {course}/>
       <Content />
       <Total />  
-      <p>iam mad</p>
+      
     </div>
   
       
