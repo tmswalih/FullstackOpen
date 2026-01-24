40ec6aeea6b93cea4e594a8f374bb60a248888c6
*tm <tmswalih953@gmail.com>
2026-01-24 07:35:55 +0530

    exercise 1.2:course information completed

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -7,24 +7,55 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+
+const PartA = (props) => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
   return(
     <div>
       <p>
         {part1} {exercises1}
       </p>
+    </div>
+  )
+}
+
+const PartB = (props) => {
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  return(
+    <div>
       <p>
         {part2} {exercises2}
       </p>
+    </div>
+  )
+}
+
+const PartC = (props) => {
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return(
+    <div>
       <p>
         {part3} {exercises3}
       </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  
+  
+  
+  return(
+    <div>  
+      <PartA />
+      <PartB />
+      <PartC />
+     
+      
+      
     </div>
   )
 }
