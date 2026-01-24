6b587014d8abc244790ff3e98b5c0048004f2dd4
*tm <tmswalih953@gmail.com>
2026-01-24 22:44:45 +0530

    1.4 => Course information step 4 completed

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = (props) => {
-  
+  console.log(props)
   return(
     <div>
       <h1>{props.course}</h1>
@@ -9,12 +9,10 @@ const Header = (props) => {
 
 
 const PartA = (props) => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
   return(
     <div>
       <p>
-        {part1} {exercises1}
+        {props.name} {props.exercises}
       </p>
     </div>
   )
@@ -26,7 +24,7 @@ const PartB = (props) => {
   return(
     <div>
       <p>
-        {part2} {exercises2}
+        {props.name} {props.exercises}
       </p>
     </div>
   )
@@ -38,21 +36,21 @@ const PartC = (props) => {
   return(
     <div>
       <p>
-        {part3} {exercises3}
+        {props.name} {props.exercises}
       </p>
     </div>
   )
 }
 
 const Content = (props) => {
-  
+  console.log(props)
   
   
   return(
     <div>  
-      <PartA />
-      <PartB />
-      <PartC />
+      <PartA name = {props.parts[0].name} exercises= {props.parts[0].exercises} />
+      <PartB name = {props.parts[1].name} exercises= {props.parts[1].exercises} />
+      <PartC name = {props.parts[2].name} exercises= {props.parts[2].exercises} />
      
       
       
@@ -60,14 +58,12 @@ const Content = (props) => {
   )
 }
 
-const Total = () => {
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
+const Total = (props) => {
   return(
     <div>
-     <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+     <p>Number of exercises {(props.parts[0].exercises) + (props.parts[1].exercises) + (props.parts[2].exercises)}</p>
     </div>
+    
   )
 }
 
@@ -75,29 +71,33 @@ const Total = () => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  
-  
+  const parts = [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]
 
   return (
     
     <div>
-
+      
       <Header course = {course}/>
-      <Content />
-      <Total />  
+      <Content parts = {parts}/>
+      <Total parts = {parts}  />
       
     </div>
   
-      
-      
-     
   )
 }
 
+      
 export default App
