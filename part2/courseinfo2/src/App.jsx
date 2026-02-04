e080fbbce99e4ed5aa052a962d332957fae07783
*tm <tmswalih953@gmail.com>
2026-02-04 14:52:27 +0530

    courseInfo2 completed

--- part2/courseinfo2/src/App.jsx
+++ part2/courseinfo2/src/App.jsx
@@ -1,42 +1,4 @@
-const Course = ({course}) => {
-  return(
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-      
-        
-      
-    </div>
-
-  )
-}
-
-const Header = ({name}) => {
-  return(
-    <h1>{name}</h1>
-  )
-}
-
-const Content = ({parts}) => {
-  return(
-    <div>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-const Total = ({parts}) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return(
-    <h3>Total of {total} exercises</h3>
-  )
-}
-
-const Part = ({part}) => {
-  return(
-    <p>{part.name} {part.exercises}</p>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
